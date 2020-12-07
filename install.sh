0f931777e1d94e9a2ae4b03ca4587e2c8a67cf16
Nat! <nat@mulle-kybernetik.com>
2020-12-07 11:50:21 +0100

    Update install.sh
    Try to use GITHUB_REF to select prerelease version

--- install.sh
+++ install.sh
@@ -6,11 +6,21 @@ mulle-sde/mulle-test;latest"
 SDE_PROJECTS="${SDE_PROJECTS}
 mulle-sde-developer;latest" 
 
+echo "${GITHUB_REF} = ${GITHUB_REF}" >&2
+
+case "${GITHUB_REF}" in
+   prerelease)
+      MULLE_SDE_DEFAULT_VERSION="${MULLE_SDE_DEFAULT_VERSION:-${GITHUB_REF}}"
+   ;;
+   
+   *)
+      MULLE_SDE_DEFAULT_VERSION="${MULLE_SDE_DEFAULT_VERSION:-release}"
+   ;;
+esac
+
+export MULLE_SDE_DEFAULT_VERSION
 export SDE_PROJECTS
 export OTHER_PROJECTS
-
-MULLE_SDE_DEFAULT_VERSION="${MULLE_SDE_DEFAULT_VERSION:-release}"
-export MULLE_SDE_DEFAULT_VERSION
 
 curl -L -O 'https://raw.githubusercontent.com/mulle-sde/mulle-sde/${MULLE_SDE_DEFAULT_VERSION}/bin/installer-all' && \
 chmod 755 installer-all && \
